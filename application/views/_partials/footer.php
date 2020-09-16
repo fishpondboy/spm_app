ae4d75da282fd4dd98c50a28aacb68bea32d5c6f
fishpondboy <jethro.kapantow@gmail.com>
2020-09-16 15:03:32 +0800

    Templating

---
+++ application/views/_partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; <?php echo Date('Y') == '2020' ? SITE_NAME . " " . Date('Y') : SITE_NAME . " 2020-" . Date('Y')  ?></span>
+        </div>
+    </div>
+</footer>
